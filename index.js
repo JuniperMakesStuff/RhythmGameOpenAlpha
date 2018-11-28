e3f4cd39e14102ac8d198e109b7d31a65ad6ef13
JuniperMakesStuff <45408795+JuniperMakesStuff@users.noreply.github.com>
2018-11-28 00:52:59 +0000

    Update index.js

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ window.onload = function() {
 
     function renderFrame() {
       
-      /*var gamepadsa = navigator.getGamepads()
+      var gamepadsa = navigator.getGamepads()
     console.log(gamepadsa[0].connected)
     console.log(gamepadsa[0].axes)
       
@@ -174,7 +174,7 @@ window.onload = function() {
   mmy = mmy/(dist/50);
   }
   }
-      }*/
+      }
       
       
       
